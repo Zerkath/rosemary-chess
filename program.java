1db5cc4d78fced952c2ee42851587b03e02b976d
EsaMaasio <esa.maasio@gmail.com>
2021-04-17 18:06:28 +0300

    king moves done

--- program.java
+++ program.java
@@ -4,7 +4,7 @@ public class program {
         Scanner scn = new Scanner(System.in);
             Game game = new Game();
 
-        game.parseFen("rnbqkbnr/pppppppp/2P5/1P6/3BNr2/3q4/PP2PPPP/RNBQKBNR w KQkq - 0 1");
+        game.parseFen("rnbqkbnr/pppppppp/2P5/1P6/3BNr2/3qK3/PP2PPPP/RNBQKBNR w KQkq - 0 1");
 
         game.printBoard();
 
