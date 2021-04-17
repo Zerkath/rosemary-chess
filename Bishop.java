b042dabe19f62e5ed727e9b855231d6691b3bb3d
Esak3 <47503144+Esak3@users.noreply.github.com>
2021-04-17 17:58:50 +0300

    queen piece done

--- Bishop.java
+++ Bishop.java
@@ -13,7 +13,72 @@ public class Bishop extends Piece {
     public ArrayList<int[]> getPossibleMoves() {
 
         ArrayList<int[]> moves = new ArrayList<>();
+        int destRow, destCol;
 
+        //Bishop moves down and right
+        for(int i = 1; row + i <= 7 && col + i <= 7; i++) { //Runs as long as destination is within board limits
+            destRow = row + i;
+            destCol = col + i;
+            if(isMovePossible(destRow, destCol)) { //Checks if within board limits and
+                moves.add(new int[]{destRow, destCol});
+
+                //if destination square has enemy piece that results in capture, breaks loop
+                if(game.board[destRow][destCol] != null) {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+
+        //Bishop moves down and left
+        for(int i = 1; row + i <= 7 && col - i >= 0; i++) {
+            destRow = row + i;
+            destCol = col - i;
+            if(isMovePossible(destRow, destCol)) {
+                moves.add(new int[]{destRow, destCol});
+
+                if(game.board[destRow][destCol] != null) {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+
+        //Bishop moves up and right
+        for(int i = 1; row - i >= 0 && col + i <= 7; i++) {
+            destRow = row - i;
+            destCol = col + i;
+            if(isMovePossible(destRow, destCol)) {
+                moves.add(new int[]{destRow, destCol});
+
+                if(game.board[destRow][destCol] != null) {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+
+        //Bishop moves up and left
+        for(int i = 1; row - i >= 0 && col - i >= 0; i++) {
+            destRow = row - i;
+            destCol = col - i;
+            if(isMovePossible(destRow, destCol)) {
+                moves.add(new int[]{destRow, destCol});
+
+                if(game.board[destRow][destCol] != null) {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+
+
+
+        /*
         int counter = 1; // The counter of traveled squares on a diagonal
         //checks possible moves for down and right diagonal
         while(true) {
@@ -95,6 +160,8 @@ public class Bishop extends Piece {
             }
             counter ++;
         }
+
+         */
 
         return moves;
     }
