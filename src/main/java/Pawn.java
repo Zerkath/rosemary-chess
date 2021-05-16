68df49b6c44dba3e2097487974b8d08fb3ca23ec
Juha <juha.airaksinen1@gmail.com>
2021-05-16 13:37:44 +0300

    cleaned pawn class

--- src/main/java/Pawn.java
+++ src/main/java/Pawn.java
@@ -11,57 +11,62 @@ public class Pawn extends Piece {
         }
     }
 
+    boolean promotable = false;
+
     //White Starting square row is 6, and black is 1 
     
     public ArrayList<int[]> getPossibleMoves() {
         ArrayList<int[]> moves = new ArrayList<>();
-        if(this.isWhite) { //if white the first index of the array goes down, left is towards 0 and right is towards 7
-            int nextRow = row - 1;
-            //move forward white
-            if(nextRow > 0 && game.board[nextRow][col] == null) {
-                moves.add(new int[]{nextRow, col});
-                if(this.row == 6 && game.board[row - 2][col] == null) { //if at starting square and nothing in front
-                    moves.add(new int[]{row -2, col}); //can jump 2 squares TODO pin checks
-                }
-            }
+        int nextRow = row;
+        int doubleJump = row;
+        int enPassantRow;
 
-            if(col > 0 && game.board[nextRow][col-1] != null && !game.board[nextRow][col-1].isWhite) {
-                moves.add(new int[]{nextRow, col-1});
+        if(this.isWhite) {
+            nextRow -= 1;
+            doubleJump -=2;
+            enPassantRow = 3;
+            if(nextRow == 0) {
+                promotable = true;
+                return moves;
             }
-
-            if(col < 7 && game.board[nextRow][col+1] != null && !game.board[nextRow][col+1].isWhite) {
-                moves.add(new int[]{nextRow, col+1});
-            }
-
-            if(row == 3) { //en passant behavior
-                if(col > 0 && game.board[row][col-1] != null && !game.board[row][col-1].isWhite && (game.board[row][col-1] instanceof Pawn)) {
-//                    System.out.println("to the left is a pawn"); //TODO check if last move from opponent was 2 squares
-                }
-                if(col < 7 && game.board[row][col+1] != null && !game.board[row][col+1].isWhite && (game.board[row][col+1] instanceof Pawn)) {
-//                    System.out.println("to the right is a pawn");
-                }
-            }
-            //TODO google en passant
-
         } else {
-            //move forward black
-            int nextRow = row + 1;
-
-            if(nextRow < 8 && game.board[nextRow][col] == null) {
-                moves.add(new int[]{nextRow, col});
-                if(this.row == 1 && game.board[row + 2][col] == null) {
-                    moves.add(new int[]{row + 2, col}); // TODO pin checks 
-                }
+            nextRow += 1;
+            doubleJump +=2;
+            enPassantRow = 4;
+            if(nextRow == 7) {
+                promotable = true;
+                return moves;
             }
+        }
 
-            if(col > 0 && game.board[nextRow][col-1] != null && game.board[nextRow][col-1].isWhite) {
-                moves.add(new int[]{nextRow, col-1});
+        boolean leftEdge = col == 0;
+        boolean rightEdge = col == 7;
+
+
+        //forward
+        if(isDestinationEmpty(nextRow, col)) {
+            moves.add(new int[]{nextRow, col});
+            if(this.row == 6 && isDestinationEmpty(doubleJump, col)) { //if at starting square and nothing in front
+                moves.add(new int[]{doubleJump, col}); //can jump 2 squares
             }
+        }
 
-            if(col < 7 && game.board[nextRow][col+1] != null && game.board[nextRow][col+1].isWhite) {
-                moves.add(new int[]{nextRow, col+1});
+        if(!leftEdge && isDestOpposing(nextRow, col-1)) {
+            moves.add(new int[]{nextRow, col-1});
+        }
+
+        if(!rightEdge && isDestOpposing(nextRow, col+1)) {
+            moves.add(new int[]{nextRow, col+1});
+        }
+
+
+        if(row == enPassantRow) { //en passant behavior
+            if(!leftEdge && isDestOpposing(row, col-1) && (game.board[row][col-1] instanceof Pawn)) {
+//                    System.out.println("to the left is a pawn"); //TODO check if last move from opponent was 2 squares
             }
-
+            if(!rightEdge && isDestOpposing(row, col+1) && (game.board[row][col+1] instanceof Pawn)) {
+//                    System.out.println("to the right is a pawn");
+            }
         }
         return moves;
     }
