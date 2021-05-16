28abef620ded858e263ab53a1475d8a8288ba22e
Zerkath <juha.airaksinen1@gmail.com>
2021-05-16 13:37:44 +0300

    cleaned pawn class

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -3,9 +3,9 @@ public class Program {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         Game game = new Game();
-        Evaluation eval = new Evaluation(16);
+        Evaluation eval = new Evaluation(1);
 
-        game.parseFen("k7/p7/8/8/8/8/1P6/K7 w - - 0 1");
+        game.parseFen("8/p6/8/8/8/8/1P6/8 w - - 0 1");
         System.out.println(game.toFenString());
         game.printBoard();
         eval.assignNewTask(game);
