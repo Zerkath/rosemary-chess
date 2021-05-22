b724d0787d61aa9ab71c027dcd848609c9f777eb
Zerkath <24889977+Zerkath@users.noreply.github.com>
2021-05-22 20:30:35 +0300

    passing tests, almost functional

--- src/test/java/MoveGenerationTests.java
+++ src/test/java/MoveGenerationTests.java
@@ -1,36 +1,29 @@
 import org.junit.jupiter.api.*;
 public class MoveGenerationTests {
-    Game game = new Game();
-    Utils utils = new Utils();
+    BoardState game = new BoardState();
     String d_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
     @Test
     void whiteStartMoves() {
-        game.parseFen(d_fen);
-        game.getPossibleMovesForTurn();
-        game.printBoard();
-        Assertions.assertEquals(20, game.moves.size());
+        game = new BoardState(Utils.parseFen(d_fen));
+        Moves moves = MoveGenerator.getAllMoves(game);
+        Assertions.assertEquals(20, moves.size());
     }
 
     @Test
     void blackStartMoves() {
-        game.parseFen(d_fen);
-        int [][] move = utils.parseCommand("d2d4");
-        game.movePiece(move);
-        game.getPossibleMovesForTurn();
-        game.printBoard();
-        Assertions.assertEquals(20, game.moves.size());
+        game = new BoardState(Utils.parseFen(d_fen));
+        game.movePiece(Utils.parseCommand("d2d4"));
+        Moves moves = MoveGenerator.getAllMoves(game);
+        Assertions.assertEquals(20, moves.size());
     }
 
     @Test
     void whiteMovesAfter_d2d4_e7e5() {
-        game.parseFen(d_fen);
-        int [][] move = utils.parseCommand("d2d4");
-        game.movePiece(move[0], move[1]);
-        move = utils.parseCommand("e7e5");
-        game.movePiece(move[0], move[1]);
-        game.getPossibleMovesForTurn();
-        game.printBoard();
-        Assertions.assertEquals(29, game.moves.size());
+        game = new BoardState(Utils.parseFen(d_fen));
+        game.movePiece(Utils.parseCommand("d2d4"));
+        game.movePiece(Utils.parseCommand("e7e5"));
+        Moves moves = MoveGenerator.getAllMoves(game);
+        Assertions.assertEquals(29, moves.size());
     }
 }
--- src/test/java/UCI_Test.java
+++ src/test/java/UCI_Test.java
@@ -6,20 +6,20 @@ public class UCI_Test {
     void kingSideCastling() {
         String pgn = "position startpos moves d2d4 e7e5 d4e5 d7d6 e5d6 c7d6 d1d6 f8d6 h2h3 d6e5 b1c3 e5c3 b2c3 d8a5 c1d2 a5a3 e2e3 c8f5 f1d3 f5d3 c2d3 g8f6 d3d4 f6e4 g1e2 e8g8 e1g1 e4d2 f1e1";
         uci.handleMessage(pgn);
-        Assertions.assertEquals("rn3rk1/pp3ppp/8/8/3P4/q1P1P2P/P2nNPP1/R3R1K1 b - - 1 15", uci.game.toFenString());
+        Assertions.assertEquals("rn3rk1/pp3ppp/8/8/3P4/q1P1P2P/P2nNPP1/R3R1K1 b - - 1 15", uci.getFen());
     }
 
     @Test
     void queenSideCastling() {
         String pgn = "position startpos moves b2b3 b7b6 c1b2 c8b7 b1c3 b8c6 e2e3 e7e6 d1e2 d8e7 e1c1 e8c8";
         uci.handleMessage(pgn);
-        Assertions.assertEquals("2kr1bnr/pbppqppp/1pn1p3/8/8/1PN1P3/PBPPQPPP/2KR1BNR w - - 4 7", uci.game.toFenString());
+        Assertions.assertEquals("2kr1bnr/pbppqppp/1pn1p3/8/8/1PN1P3/PBPPQPPP/2KR1BNR w - - 4 7", uci.getFen());
     }
 
     @Test
     void noCastling() {
         String pgn = "position startpos moves e2e4 e7e5 e1e2";
         uci.handleMessage(pgn);
-        Assertions.assertEquals("rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPPKPPP/RNBQ1BNR b kq - 1 2", uci.game.toFenString());
+        Assertions.assertEquals("rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPPKPPP/RNBQ1BNR b kq - 1 2", uci.getFen());
     }
 }
