288336fda3f4cf7d7b30cab9c22de6e3d9883ffc
Zerkath <24889977+Zerkath@users.noreply.github.com>
2021-06-01 14:39:42 +0300

    fixed up mate in turns print outs to better represent the turns needed

---
+++ src/test/java/EvalTests.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.*;
+public class EvalTests {
+    @Test
+    void equalStart() {
+        BoardState boardState = new BoardState(Utils.parseFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"));
+        int eval = Evaluation.calculate(boardState);
+        Assertions.assertEquals(0, eval);
+    }
+    @Test
+    void noPawns() {
+        BoardState boardState = new BoardState(Utils.parseFen("rnbqkbnr/8/8/8/8/8/8/RNBQKBNR w KQkq - 0 1"));
+        int eval = Evaluation.calculate(boardState);
+        Assertions.assertEquals(0, eval);
+    }
+
+    @Test
+    void noPawnsBlack() {
+        BoardState boardState = new BoardState(Utils.parseFen("rnbqkbnr/8/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"));
+        int eval = Evaluation.calculate(boardState);
+        Assertions.assertEquals(800, eval);
+    }
+
+    @Test
+    void noPawnsWhite() {
+        BoardState boardState = new BoardState(Utils.parseFen("rnbqkbnr/pppppppp/8/8/8/8/8/RNBQKBNR w KQkq - 0 1"));
+        int eval = Evaluation.calculate(boardState);
+        Assertions.assertEquals(-800, eval);
+    }
+
+    @Test
+    void whiteMoreMiddleControl() {
+        BoardState boardState = new BoardState(Utils.parseFen("rnbqkbnr/3pp3/8/8/8/3PP3/8/RNBQKBNR w KQkq - 0 1"));
+        int eval = Evaluation.calculate(boardState);
+        Assertions.assertTrue(eval > 0);
+    }
+
+    @Test
+    void blackMoreMiddleControl() {
+        BoardState boardState = new BoardState(Utils.parseFen("rnbqkbnr/8/3pp3/8/8/8/3PP3/RNBQKBNR w KQkq - 0 1"));
+        int eval = Evaluation.calculate(boardState);
+        Assertions.assertTrue(eval < 0);
+    }
+}
