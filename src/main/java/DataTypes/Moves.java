84afe2ad6e4a70fd8e2d072a8851defe0a8caac8
Juha <juha.airaksinen1@gmail.com>
2021-09-06 22:41:44 +0300

    Moved some functionality away from Utils
    - Migrating functionality inside classes instead of having a static
      class to operate on data

--- src/main/java/DataTypes/Moves.java
+++ src/main/java/DataTypes/Moves.java
@@ -1,14 +1,12 @@
 package DataTypes;
 
-import CommonTools.Utils;
-
 import java.util.LinkedList;
 
 public class Moves extends LinkedList<Move> {
     public String getString() {
         StringBuilder stringBuilder = new StringBuilder();
         for (Move move : this) {
-            stringBuilder.append(Utils.parseCommand(move));
+            stringBuilder.append(move.toString()).append(": 1");
             stringBuilder.append('\n');
         }
         return stringBuilder.toString();
--- src/test/java/UtilsTest.java
+++ src/test/java/UtilsTest.java
@@ -1,21 +1,31 @@
 import CommonTools.Utils;
 import DataTypes.Coordinate;
+import DataTypes.Move;
 import org.junit.jupiter.api.*;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UtilsTest {
-    @Test
-    void characters() {
-        for (int i = 0; i < 8; i++) {
-            char c = (char)(i+'a');
-            Assertions.assertEquals(c, Utils.toColumnCharacter(Utils.toColumnNumber(c)));
-        }
-    }
 
     @Test
     void coordinate() {
         Coordinate expected = new Coordinate(4, 4);
-        Coordinate result = Utils.parseCoordinate("e4");
+        Coordinate result = new Coordinate("e4");
         Assertions.assertEquals(expected.column, result.column);
         Assertions.assertEquals(expected.row, result.row);
+    }
+
+    @Test
+    void move() {
+        Move expected = new Move(new Coordinate(4, 4), new Coordinate(3, 4));
+        Move result = new Move("e4e5");
+        Assertions.assertEquals(expected.destination.column, result.destination.column);
+        Assertions.assertEquals(expected.destination.row, result.destination.row);
+        Assertions.assertEquals(expected.origin.column, result.origin.column);
+        Assertions.assertEquals(expected.origin.row, result.origin.row);
+    }
+
+    @Test
+    void promotionCommand() {
+        Move expected = new Move(new Move(new Coordinate(1, 7), new Coordinate(0, 7)), 'N');
+        Assertions.assertEquals(expected.promotion, new Move("h7h8N").promotion);
     }
 }
