2dfad752a0ad8784e100ea215a4d19ff561a0d0d
Zerkath <24889977+Zerkath@users.noreply.github.com>
2021-09-16 21:02:47 +0300

    Changing from Object pieces to integers
    - Trying to stray away from unneccessary objects to reduce heap size so
      gc doesn't run as often
    - After changes perft performance increased for depth 6 by another 50s,
      wsl got a 10s increase
    - All tests pass
    - Eval not up to date

--- src/main/java/DataTypes/Move.java
+++ src/main/java/DataTypes/Move.java
@@ -3,34 +3,33 @@ package DataTypes;
 public class Move {
     public Coordinate origin;
     public Coordinate destination;
-    public Piece promotion;
+    public int promotion;
 
-    //todo rework promotion into a piece instead of char
     public Move(Coordinate origin, Coordinate destination) {
         this.origin = origin;
         this.destination = destination;
-        this.promotion = null;
+        this.promotion = 0;
     }
 
-    public Move(Move move, char promotion) {
+    public Move(Move move, int promotion) {
         this.origin = move.origin;
         this.destination = move.destination;
-        this.promotion = new Piece(promotion);
+        this.promotion = promotion;
     }
 
     public Move(String move) {
         origin = new Coordinate(move.substring(0, 2));
         destination = new Coordinate(move.substring(2, 4));
-        promotion = null;
+        promotion = 0;
         if(move.length() == 5) {
-            promotion = new Piece(move.charAt(4));
+            promotion = Pieces.getNum(move.charAt(4));
         }
     }
 
     public String toString() {
         String sMove = origin.toString() + destination.toString();
-        if(promotion != null) {
-            sMove += promotion.toChar();
+        if(promotion != 0) {
+            sMove += Pieces.getChar(promotion);
         }
         return sMove;
     }
