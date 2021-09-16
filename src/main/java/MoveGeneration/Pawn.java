dee993b95dc0a21fa07c2510d1f5e9a82f78a5c8
Juha <juha.airaksinen1@gmail.com>
2021-09-16 21:02:47 +0300

    Changing from Object pieces to integers
    - Trying to stray away from unneccessary objects to reduce heap size so
      gc doesn't run as often
    - After changes perft performance increased for depth 6 by another 50s,
      wsl got a 10s increase
    - All tests pass
    - Eval not up to date

--- src/main/java/MoveGeneration/Pawn.java
+++ src/main/java/MoveGeneration/Pawn.java
@@ -9,19 +9,14 @@ public class Pawn implements PieceGenerator {
 
     private Moves pawnPromotions(Move move, boolean isWhite) {
         Moves moves = new Moves();
-        char queen, knight, rook, bishop;
+        int offset = isWhite ? Pieces.WHITE : Pieces.BLACK;
 
-        if(isWhite) {
-            queen = 'Q'; knight = 'N'; rook = 'R'; bishop = 'B';
-        } else {
-            queen = 'q'; knight = 'n'; rook = 'r'; bishop = 'b';
-        }
         moves.addAll(
                 Arrays.asList(
-                        new Move(move, queen),
-                        new Move(move, knight),
-                        new Move(move, rook),
-                        new Move(move, bishop)
+                        new Move(move, Pieces.QUEEN | offset),
+                        new Move(move, Pieces.KNIGHT | offset),
+                        new Move(move, Pieces.ROOK | offset),
+                        new Move(move, Pieces.BISHOP | offset)
                 )
         );
         return moves;
@@ -32,8 +27,8 @@ public class Pawn implements PieceGenerator {
         Board board = boardState.board;
 
         Moves moves = new Moves();
-        Piece orig = board.getCoordinate(origin);
-        boolean isWhite = orig.isWhite();
+        int original_piece = board.getCoordinate(origin);
+        boolean isWhite = Pieces.isWhite(original_piece);
         boolean promotion = false;
         int row = origin.row;
         int col = origin.column;
@@ -58,13 +53,13 @@ public class Pawn implements PieceGenerator {
             }
         }
 
+        if(nextRow < 0 || nextRow > 7) return moves; // if nextRow == -1 / 8
+
         boolean leftEdge = col == 0;
         boolean rightEdge = col == 7;
 
-        if(!board.isCoordinateInBounds(new Coordinate(nextRow, col))) return moves; // if nextRow == -1 / 8
-
         //forward
-        if(board.locationIsEmpty(nextRow, col)) {
+        if(board.getCoordinate(nextRow, col) == Pieces.EMPTY) {
             Move nextMove = new Move(origin, new Coordinate(nextRow, col));
             if(promotion) {
                 moves.addAll(pawnPromotions(nextMove, isWhite));
@@ -73,7 +68,7 @@ public class Pawn implements PieceGenerator {
             }
 
             if((isWhite && row == 6 || (!isWhite && row == 1))
-                    && board.locationIsEmpty(doubleJump, col)) {
+                    && board.getCoordinate(doubleJump, col) == Pieces.EMPTY) {
                 //if at starting square and nothing in front
                 Coordinate destination = new Coordinate(doubleJump, col);
                 moves.add(new Move(origin, destination));
@@ -81,7 +76,7 @@ public class Pawn implements PieceGenerator {
         }
 
         //captures
-        if(!leftEdge && board.pawnCapturePossible(new Coordinate(nextRow, col-1), orig)) {
+        if(!leftEdge && board.pawnCapturePossible(new Coordinate(nextRow, col-1), original_piece)) {
             Move nextMove = new Move(origin, new Coordinate(nextRow, col-1));
             if(promotion) {
                 moves.addAll(pawnPromotions(nextMove, isWhite));
@@ -90,7 +85,7 @@ public class Pawn implements PieceGenerator {
             }
         }
 
-        if(!rightEdge && board.pawnCapturePossible(new Coordinate(nextRow, col+1), orig)) {
+        if(!rightEdge && board.pawnCapturePossible(new Coordinate(nextRow, col+1), original_piece)) {
             Move nextMove = new Move(origin, new Coordinate(nextRow, col+1));
             if(promotion) {
                 moves.addAll(pawnPromotions(nextMove, isWhite));
