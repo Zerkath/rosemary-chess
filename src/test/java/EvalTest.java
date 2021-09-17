12dce0b37c109ff68f7d6522bb69b952e5792174
Zerkath <24889977+Zerkath@users.noreply.github.com>
2021-09-18 00:08:02 +0300

    Fixed evaluation
    - Fixed evaluation by counting pieces before calculating value
    - For some reason doesn't play nicely with UCI gui's
    - Maybe issue with line endings, currently using only LF maybe should
      try CRLF

--- src/test/java/EvalTest.java
+++ src/test/java/EvalTest.java
@@ -1,7 +1,12 @@
 import BoardRepresentation.BoardState;
 
+import DataTypes.Pieces;
 import Evaluation.EvaluationCalculations;
+import Main.UCI_Controller;
 import org.junit.jupiter.api.*;
+
+import java.util.Map;
+
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class EvalTest {
 
@@ -46,5 +51,32 @@ public class EvalTest {
         BoardState boardState = new BoardState("rnbqkbnr/8/3pp3/8/8/8/3PP3/RNBQKBNR w KQkq - 0 1");
         int eval = evaluationCalculations.calculateMaterial(boardState);
         Assertions.assertTrue(eval < 0);
+    }
+
+    @Test
+    void pieceCountsMatch() {
+        BoardState boardState = new BoardState("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+        System.out.println("Start");
+        boardState.updatePieceCount();
+        for (Map.Entry<Integer, Integer> entry: boardState.pieceMap.entrySet()) {
+            System.out.println(Pieces.getChar(entry.getKey()) + " count: " + entry.getValue());
+        }
+        UCI_Controller uci = new UCI_Controller();
+        uci.boardState = boardState;
+        uci.runPerft(4, false);
+        boardState.updatePieceCount();
+        Map<Integer, Integer> map = uci.boardState.pieceMap;
+        System.out.println("\nAfter perft");
+        for (Map.Entry<Integer, Integer> entry: map.entrySet()) {
+            System.out.println(Pieces.getChar(entry.getKey()) + " count: " + entry.getValue());
+        }
+        Assertions.assertEquals(8, map.get(Pieces.PAWN | Pieces.WHITE));
+        Assertions.assertEquals(8, map.get(Pieces.PAWN | Pieces.BLACK));
+        Assertions.assertEquals(2, map.get(Pieces.KNIGHT | Pieces.WHITE));
+        Assertions.assertEquals(2, map.get(Pieces.KNIGHT | Pieces.BLACK));
+        Assertions.assertEquals(2, map.get(Pieces.ROOK | Pieces.BLACK));
+        Assertions.assertEquals(2, map.get(Pieces.ROOK | Pieces.WHITE));
+        Assertions.assertEquals(1, map.get(Pieces.KING | Pieces.BLACK));
+        Assertions.assertEquals(1, map.get(Pieces.KING | Pieces.WHITE));
     }
 }
