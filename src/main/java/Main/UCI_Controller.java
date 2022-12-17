47f09d63df2ef9f2a6f7a990a60d64a6a6de568c
Zerkath <juha.airaksinen1@gmail.com>
2022-12-18 00:30:51 +0200

    feat: Async work for perft 2x speedup

--- src/main/java/Main/UCI_Controller.java
+++ src/main/java/Main/UCI_Controller.java
@@ -11,7 +11,9 @@ import MoveGeneration.MoveGenerator;
 import java.io.BufferedOutputStream;
 import java.util.*;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.stream.Collectors;
 
 public class UCI_Controller extends OutputUtils {
     public BoardState boardState;
@@ -40,54 +42,58 @@ public class UCI_Controller extends OutputUtils {
     static class waitForReady implements Runnable {
 
         BlockingQueue<String> queue;
+
         public waitForReady(BlockingQueue<String> queue) {
             this.queue = queue;
         }
 
         @Override
         public void run() {
-            while(!queue.isEmpty()) {
+            while (!queue.isEmpty()) {
                 try {
                     wait(500);
-                } catch (InterruptedException ignored) {}
+                } catch (InterruptedException ignored) {
+                }
             }
             System.out.println("readyok");
         }
     }
 
-    public void handleMessage(String message, BlockingQueue<String> queue) { //todo rework to tokenize the commands given by the ui (change to switch case)
-        String [] split = message.split(" ");
-        if(message.equals("uci")) {
+    public void handleMessage(String message, BlockingQueue<String> queue) { // todo rework to tokenize the commands
+                                                                             // given
+                                                                             // by the ui (change to switch case)
+        String[] split = message.split(" ");
+        if (message.equals("uci")) {
             setToUCI();
             return;
         }
-        if(split[0].equals("position")) {
-            if(split[1].equals("fen")) {
+        if (split[0].equals("position")) {
+            if (split[1].equals("fen")) {
                 int startIndex = message.indexOf('"') + 1;
-                int endIndex = message.indexOf('"', startIndex+1);
+                int endIndex = message.indexOf('"', startIndex + 1);
                 setFen(message.substring(startIndex, endIndex));
-                if(endIndex+2 < message.length()) {
-                    String [] moves = message.substring(endIndex+2).split(" ");
+                if (endIndex + 2 < message.length()) {
+                    String[] moves = message.substring(endIndex + 2).split(" ");
                     moves = Arrays.copyOfRange(moves, 1, moves.length);
                     boardState.playMoves(moves);
                 }
                 return;
             }
 
-            if(split[1].equals("startpos")) {
+            if (split[1].equals("startpos")) {
                 setToDefault();
-                String [] moves = Arrays.copyOfRange(split, 3, split.length);
+                String[] moves = Arrays.copyOfRange(split, 3, split.length);
                 boardState.playMoves(moves);
                 return;
             }
         }
-        if(split[0].equals("isready")) {
+        if (split[0].equals("isready")) {
             readyResponse(queue);
             return;
         }
-        if(split[0].equals("go")) {
-            if(split.length > 2) {
-                if(split[1].equals("perft")) {
+        if (split[0].equals("go")) {
+            if (split.length > 2) {
+                if (split[1].equals("perft")) {
                     int depth = Integer.parseInt(split[2]);
                     getPerft(depth);
                     return;
@@ -96,48 +102,49 @@ public class UCI_Controller extends OutputUtils {
             startEval();
             return;
         }
-        if(split[0].equals("stop")) {
+        if (split[0].equals("stop")) {
             endEval();
             return;
         }
-        if(split[0].equals("setoption")) {
-            if(split.length >= 4) {
-                if(split[2].equals("depth")) {
+        if (split[0].equals("setoption")) {
+            if (split.length >= 4) {
+                if (split[2].equals("depth")) {
                     this.depth = Integer.parseInt(split[4]);
                     return;
                 }
             }
         }
-        if(split[0].equals("quit")) {
+        if (split[0].equals("quit")) {
             System.exit(0);
         }
-        if(split[0].equals("debug")) {
+        if (split[0].equals("debug")) {
             debug = split[1].equals("on");
             return;
         }
-        if(split[0].equals("register")) {
+        if (split[0].equals("register")) {
             println("register later");
             return;
         }
-        if(split[0].equals("ucinewgame")) {
+        if (split[0].equals("ucinewgame")) {
             setToDefault();
             return;
         }
-        if(split[0].equals("xboard")) return;
+        if (split[0].equals("xboard"))
+            return;
         println("?");
     }
-
 
     public void setFen(String fen) {
         boardState = new BoardState(FenUtils.parseFen(fen));
     }
+
     public String getFen() {
         return boardState.toFenString();
     }
 
     public void setToUCI() {
         uci_mode = true;
-//        System.out.print("option name Threads type spin default 2 min 1 max 250\n");
+        // System.out.print("option name Threads type spin default 2 min 1 max 250\n");
         println("id name Rosemary");
         println("id author Rosemary_Devs");
         println("option name depth type spin default 6 min 1 max 8");
@@ -145,7 +152,7 @@ public class UCI_Controller extends OutputUtils {
     }
 
     public void readyResponse(BlockingQueue<String> queue) {
-        new Thread(new waitForReady(queue)).start(); //should wait for queue to empty
+        new Thread(new waitForReady(queue)).start(); // should wait for queue to empty
     }
 
     public void startEval() {
@@ -153,11 +160,13 @@ public class UCI_Controller extends OutputUtils {
     }
 
     public void endEval() {
-        if(threadGroup.activeCount() > 0) threadGroup.interrupt();
+        if (threadGroup.activeCount() > 0)
+            threadGroup.interrupt();
     }
 
     public void startEval(int depth) {
-        if(threadGroup.activeCount() < 1) new Thread(threadGroup, new EvaluationThread(this.boardState, depth, debug, writer)).start();
+        if (threadGroup.activeCount() < 1)
+            new Thread(threadGroup, new EvaluationThread(this.boardState, depth, debug, writer)).start();
     }
 
     public void getPerft(int depth) {
@@ -175,16 +184,63 @@ public class UCI_Controller extends OutputUtils {
         return perft(depth, depth, print, boardState);
     }
 
+    private class PerftResult {
+        Integer moveCount;
+        Move move;
+
+        PerftResult(Integer count, Move move) {
+            this.moveCount = count;
+            this.move = move;
+        }
+
+        void toStdOut(boolean print) {
+            if (print)
+                println(move + ":" + moveCount);
+        }
+    }
+
     private int perft(int depth, int start, boolean print, BoardState boardState) {
-        if(depth <= 0) return 1;
 
-        Moves moves = moveGenerator.getLegalMoves(boardState);
+        List<CompletableFuture<PerftResult>> list = moveGenerator
+                .getLegalMoves(boardState)
+                .stream()
+                .filter(m -> m != null)
+                .map(move -> CompletableFuture.supplyAsync(() -> new PerftResult(perftProcess(
+                        depth - 1,
+                        start,
+                        boardState.makeNonModifyingMove(move)),
+                        move)))
+                .collect(Collectors.toList());
+
+        CompletableFuture<Void> futures = CompletableFuture
+                .allOf(list.toArray(new CompletableFuture[list.size()]));
+
+        CompletableFuture<Integer> result = futures.thenApply(future -> {
+            return list
+                    .stream()
+                    .map(CompletableFuture::join)
+                    .peek(pr -> pr.toStdOut(print))
+                    .map(pr -> pr.moveCount)
+                    .reduce(0, Integer::sum);
+        });
+
+        try {
+            return result.get();
+        } catch (Throwable e) {
+            return 0;
+        }
+    }
+
+    private int perftProcess(int depth, int start, BoardState boardState) {
+        if (depth <= 0)
+            return 1;
+
         int numPositions = 0;
+        Moves moves = moveGenerator.getLegalMoves(boardState);
 
-        for (Move move: moves) {
+        for (Move move : moves) {
             boardState.makeMove(move);
-            int result = perft(depth-1, start, print, boardState);
-            if(depth == start && print) println(move + ": " + result);
+            int result = perftProcess(depth - 1, start, boardState);
             numPositions += result;
             boardState.unMakeMove();
         }
