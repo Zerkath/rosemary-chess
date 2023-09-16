370fe955ba215a66282eba5b5bf8fc10d3fa58b5
Zerkath <juha.airaksinen1@gmail.com>
2023-09-16 17:41:16 +0300

    refactor: Moved packages to project package naming convention

---
+++ src/main/java/com/github/zerkath/rosemary/DataTypes/Move.java
@@ -0,0 +1,56 @@
+package com.github.zerkath.rosemary.DataTypes;
+
+/**
+ * Used for storing basic information about moves
+ * From and to where
+ * also promotion
+ *
+ * TODO: Could convert this to a int value, and use bit shifting to store the values
+ * Reason for this is that at perft 4 this accounts for 5.6% of total allocated objects
+ */
+public class Move {
+    public Coordinate origin;
+    public Coordinate destination;
+    public int promotion;
+
+    public Move(Coordinate origin, Coordinate destination) {
+        this.origin = origin;
+        this.destination = destination;
+        this.promotion = 0;
+    }
+
+    public Move(Move move, int promotion) {
+        this.origin = move.origin;
+        this.destination = move.destination;
+        this.promotion = promotion;
+    }
+
+    public Move(String move) {
+        origin = Utils.getCoordinate(move.substring(0, 2));
+        destination = Utils.getCoordinate(move.substring(2, 4));
+        promotion = 0;
+        if(move.length() == 5) {
+            promotion = Pieces.getNum(move.charAt(4));
+        }
+    }
+
+    public String toString() {
+        String sMove = origin.toString() + destination.toString();
+        if(promotion != 0) {
+            sMove += Pieces.getChar(promotion);
+        }
+        return sMove;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (!(other instanceof Move)) return false;
+        Move otherMove = (Move)other;
+        return origin.equals(otherMove.origin) && destination.equals(otherMove.destination) && promotion == otherMove.promotion;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.toString().hashCode();
+    }
+}
