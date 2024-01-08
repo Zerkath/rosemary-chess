d52e30b5a92904bd26b1a3b450214196e68f977c
Zerkath <24889977+Zerkath@users.noreply.github.com>
2024-01-08 14:04:40 +0200

    refactor: Shorter package path (#29)

--- src/main/java/com/github/zerkath/rosemary/types/TargetSquare.java
+++
@@ -1,7 +0,0 @@
-package com.github.zerkath.rosemary.types;
-
-public enum TargetSquare {
-  EMPTY,
-  FRIENDLY,
-  ENEMY
-}
---
+++ src/main/java/rosemary/types/TargetSquare.java
@@ -0,0 +1,7 @@
+package rosemary.types;
+
+public enum TargetSquare {
+  EMPTY,
+  FRIENDLY,
+  ENEMY
+}
