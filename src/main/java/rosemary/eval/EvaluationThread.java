2caccff7d2233bedbbaf3969319709c68c2f9169
Zerkath <juha.airaksinen1@gmail.com>
2024-01-10 10:08:58 +0200

    Feat/change eval (#30)
    * feat: adjust eval
    * feat: Eval position calculation change & change spotless to google aosp

--- src/main/java/rosemary/eval/EvaluationThread.java
+++ src/main/java/rosemary/eval/EvaluationThread.java
@@ -10,144 +10,126 @@ import rosemary.types.Pieces;
 
 public class EvaluationThread extends OutputUtils implements Runnable {
 
-  BoardState boardState;
-  int startingDepth;
-  boolean playerTurnWhite;
-  boolean debug;
-  int depth;
-  EvaluationValues values = new EvaluationValues();
-  EvaluationCalculations evalCalculator = new EvaluationCalculations();
-  MoveGenerator moveGenerator = new MoveGenerator();
+    BoardState boardState;
+    int startingDepth;
+    boolean playerTurnWhite;
+    boolean debug;
+    int depth;
+    EvaluationValues values = new EvaluationValues();
+    EvaluationCalculations evalCalculator = new EvaluationCalculations();
+    MoveGenerator moveGenerator = new MoveGenerator();
 
-  public EvaluationThread(
-      BoardState boardState, int depth, boolean debug, BufferedOutputStream writer) {
-    super(writer);
-    this.boardState = boardState;
-    this.startingDepth = depth;
-    this.depth = depth;
-    this.debug = debug;
-    this.playerTurnWhite = boardState.isWhiteTurn;
-  }
-
-  @Override
-  public void run() {
-    int eval;
-    eval =
-        boardState.isWhiteTurn
-            ? alphaBetaMax(boardState, Integer.MIN_VALUE, Integer.MAX_VALUE, depth)
-            : alphaBetaMin(boardState, Integer.MIN_VALUE, Integer.MAX_VALUE, depth);
-  }
-
-  int alphaBetaMax(BoardState boardState, int alpha, int beta, int depth) { // white
-
-    Moves moves = moveGenerator.getLegalMoves(boardState);
-    if (moves.isEmpty()) { // no moves this turn in checkmate or draw
-      if (inCheck(boardState)) {
-        return -values.mate + ((startingDepth - depth) / 2);
-      }
-      return 0;
+    public EvaluationThread(
+            BoardState boardState, int depth, boolean debug, BufferedOutputStream writer) {
+        super(writer);
+        this.boardState = boardState;
+        this.startingDepth = depth;
+        this.depth = depth;
+        this.debug = debug;
+        this.playerTurnWhite = boardState.isWhiteTurn;
     }
 
-    if (depth == 0 || Thread.currentThread().isInterrupted()) {
-      return evalCalculator.calculateMaterial(boardState);
+    @Override
+    public void run() {
+        int eval;
+        eval =
+                alphaBeta(
+                        boardState,
+                        Integer.MIN_VALUE,
+                        Integer.MAX_VALUE,
+                        depth,
+                        boardState.isWhiteTurn);
     }
 
-    short bestMove = -1;
+    int alphaBeta(BoardState boardState, int alpha, int beta, int depth, boolean isMaxing) {
+        Moves moves = moveGenerator.getLegalMoves(boardState);
 
-    for (short move : moves) {
-      int eval = alphaBetaMin(Mover.makeMove(boardState, move), alpha, beta, depth - 2);
-      if (depth == startingDepth) {
-        printInfoUCI(depth, eval, move, true);
-      }
-      if (eval >= beta) {
-        return beta;
-      }
-      if (eval > alpha) {
-        bestMove = move;
-        alpha = eval;
-      }
+        if (moves.isEmpty()) { // no moves this turn in checkmate or draw
+            if (inCheck(boardState)) {
+                return isMaxing
+                        ? -values.mate + ((startingDepth - depth) / 2)
+                        : values.mate - ((startingDepth - depth) / 2);
+            }
+            return 0;
+        }
+
+        if (depth == 0 || Thread.currentThread().isInterrupted()) {
+            return evalCalculator.calculateMaterial(boardState);
+        }
+
+        short bestMove = -1;
+
+        for (short move : moves) {
+            int eval =
+                    alphaBeta(Mover.makeMove(boardState, move), alpha, beta, depth - 1, !isMaxing);
+
+            if (depth == startingDepth) {
+                printInfoUCI(depth, eval, move, isMaxing);
+            }
+
+            if (isMaxing) {
+                if (eval >= beta) {
+                    return beta;
+                }
+                if (eval > alpha) {
+                    bestMove = move;
+                    alpha = eval;
+                }
+            } else {
+                if (eval <= alpha) {
+                    return alpha;
+                }
+                if (eval < beta) {
+                    bestMove = move;
+                    beta = eval;
+                }
+            }
+        }
+
+        if (depth == startingDepth) {
+            if (bestMove != -1) bestMove = moves.getFirst();
+            println("bestmove " + MoveUtil.moveToString(bestMove));
+        }
+
+        return isMaxing ? alpha : beta;
     }
 
-    if (depth == startingDepth) {
-      if (bestMove != -1) bestMove = moves.getFirst();
-      println("bestmove " + MoveUtil.moveToString(bestMove));
-    }
-    return alpha;
-  }
+    private void printInfoUCI(int depth, int eval, short move, boolean isWhite) {
 
-  private int alphaBetaMin(BoardState boardState, int alpha, int beta, int depth) { // black
+        String outString = "info depth " + depth;
+        String currMove = " currmove " + MoveUtil.moveToString(move);
 
-    Moves moves = moveGenerator.getLegalMoves(boardState);
-    if (moves.isEmpty()) { // no moves this turn in checkmate or draw
-      if (inCheck(boardState)) {
-        return values.mate - ((startingDepth - depth) / 2);
-      }
-      return 0;
-    }
+        boolean whiteHasMate = eval >= values.mateForWhite;
+        boolean isMate = whiteHasMate || eval <= values.mateForBlack;
+        int whiteTurn = isWhite ? 1 : -1;
 
-    if (depth == 0 || Thread.currentThread().isInterrupted()) {
-      return evalCalculator.calculateMaterial(boardState);
-    }
-
-    short bestMove = -1;
-
-    for (short move : moves) {
-      int eval = alphaBetaMax(Mover.makeMove(boardState, move), alpha, beta, depth - 1);
-      if (depth == startingDepth) {
-        printInfoUCI(depth, eval, move, false);
-      }
-      if (eval <= alpha) {
-        return alpha;
-      }
-      if (eval < beta) {
-        bestMove = move;
-        beta = eval;
-      }
+        if (isMate) {
+            int offset =
+                    whiteHasMate
+                            ? whiteTurn + values.mate - eval
+                            : whiteTurn - values.mate - eval - (isWhite ? 1 : 0);
+            outString += " score mate " + offset;
+        } else {
+            outString += " score cp " + eval;
+        }
+        println(outString + currMove);
     }
 
-    if (depth == startingDepth) {
-      if (bestMove != -1) bestMove = moves.getFirst();
-      println("bestmove " + MoveUtil.moveToString(bestMove));
-    }
-    return beta;
-  }
-
-  private void printInfoUCI(int depth, int eval, short move, boolean isWhite) {
-
-    String outString = "info depth " + depth;
-    String currMove = " currmove " + MoveUtil.moveToString(move);
-
-    boolean whiteHasMate = eval >= values.mateForWhite;
-    boolean isMate = whiteHasMate || eval <= values.mateForBlack;
-    int whiteTurn = isWhite ? 1 : -1;
-
-    if (isMate) {
-      int offset =
-          whiteHasMate
-              ? whiteTurn + values.mate - eval
-              : whiteTurn - values.mate - eval - (isWhite ? 1 : 0);
-      outString += " score mate " + offset;
-    } else {
-      outString += " score cp " + eval;
-    }
-    println(outString + currMove);
-  }
-
-  private boolean inCheck(BoardState state) {
-    boolean old = state.isWhiteTurn;
-    boolean isWhite = state.isWhiteTurn;
-    state.isWhiteTurn = !state.isWhiteTurn; // flip turn temporarily for checking a check
-    Moves opponent = moveGenerator.getLegalMoves(state);
-    for (short move : opponent) {
-      int piece = state.board.getCoordinate(MoveUtil.getDestination(move));
-      if (piece == 0) continue;
-      if ((isWhite && piece == (Pieces.KING | Pieces.WHITE))
-          || (!isWhite && piece == (Pieces.KING | Pieces.BLACK))) {
+    private boolean inCheck(BoardState state) {
+        boolean old = state.isWhiteTurn;
+        boolean isWhite = state.isWhiteTurn;
+        state.isWhiteTurn = !state.isWhiteTurn; // flip turn temporarily for checking a check
+        Moves opponent = moveGenerator.getLegalMoves(state);
+        for (short move : opponent) {
+            int piece = state.board.getCoordinate(MoveUtil.getDestination(move));
+            if (piece == 0) continue;
+            if ((isWhite && piece == (Pieces.KING | Pieces.WHITE))
+                    || (!isWhite && piece == (Pieces.KING | Pieces.BLACK))) {
+                state.isWhiteTurn = old;
+                return true;
+            }
+        }
         state.isWhiteTurn = old;
-        return true;
-      }
+        return false;
     }
-    state.isWhiteTurn = old;
-    return false;
-  }
 }
--- src/main/java/rosemary/generation/Knight.java
+++ src/main/java/rosemary/generation/Knight.java
@@ -5,36 +5,36 @@ import rosemary.types.*;
 
 public class Knight {
 
-  static Moves[] knightMoves = new Moves[64];
+    static Moves[] knightMoves = new Moves[64];
 
-  static {
-    for (short origin = 0; origin < 64; origin++) {
-      short row = MoveUtil.getRow(origin);
-      short col = MoveUtil.getColumn(origin);
+    static {
+        for (short origin = 0; origin < 64; origin++) {
+            short row = MoveUtil.getRow(origin);
+            short col = MoveUtil.getColumn(origin);
 
-      short[] columns = new short[] {(short) (col - 2), (short) (col + 2)};
-      short[] rows = new short[] {(short) (row - 2), (short) (row + 2)};
-      Moves moves = new Moves();
-      for (int d_column : columns) {
-        Utils.addToCollection(row + 1, d_column, row, col, moves);
-        Utils.addToCollection(row - 1, d_column, row, col, moves);
-      }
+            short[] columns = new short[] {(short) (col - 2), (short) (col + 2)};
+            short[] rows = new short[] {(short) (row - 2), (short) (row + 2)};
+            Moves moves = new Moves();
+            for (int d_column : columns) {
+                Utils.addToCollection(row + 1, d_column, row, col, moves);
+                Utils.addToCollection(row - 1, d_column, row, col, moves);
+            }
 
-      for (int d_row : rows) {
-        Utils.addToCollection(d_row, col + 1, row, col, moves);
-        Utils.addToCollection(d_row, col - 1, row, col, moves);
-      }
-      knightMoves[origin] = moves;
+            for (int d_row : rows) {
+                Utils.addToCollection(d_row, col + 1, row, col, moves);
+                Utils.addToCollection(d_row, col - 1, row, col, moves);
+            }
+            knightMoves[origin] = moves;
+        }
     }
-  }
 
-  public static void getMoves(short origin, BoardState boardState, Moves moves) {
+    public static void getMoves(short origin, BoardState boardState, Moves moves) {
 
-    Board board = boardState.board;
-    Moves n_moves = knightMoves[origin];
+        Board board = boardState.board;
+        Moves n_moves = knightMoves[origin];
 
-    for (short move : n_moves) {
-      if (board.isValidMove(move)) moves.add(move);
+        for (short move : n_moves) {
+            if (board.isValidMove(move)) moves.add(move);
+        }
     }
-  }
 }
