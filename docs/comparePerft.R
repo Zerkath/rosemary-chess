9b9f6910766d427ac88032736256fd059ee050d0
Zerkath <24889977+Zerkath@users.noreply.github.com>
2024-06-15 20:27:24 +0300

    docs: Update chart & readme

--- docs/comparePerft.R
+++ docs/comparePerft.R
@@ -1,22 +1,24 @@
 pdf(NULL)
 library(readr)
+library(dplyr)
 library(ggplot2)
-data <- read_csv("data/perft.csv")
+new <- read_csv("data/new.csv")
+old <- read_csv("data/old.csv")
 
-df_diff <- aggregate(
-  Score ~ Benchmark,
-  data = data,
-  FUN = function(x) {
-    diff(x)[1]
-  }
-)
+merged_df <- merge(new, old, by = "Benchmark", suffixes = c("_new", "_old"))
 
-df_diff <- df_diff[order(-df_diff$Score), ]
-df_diff$Benchmark <- factor(df_diff$Benchmark, levels = df_diff$Benchmark)
+# Calculate the difference in scores
+merged_df <- merged_df %>%
+  mutate(Score_Diff = Score_new - Score_old) %>%
+  select(Benchmark, Score_Diff)
 
-print(df_diff)
+merged_df$Benchmark <- sub("^rosemary.PerftBenchmark.position_", "", merged_df$Benchmark)
+merged_df <- merged_df[order(-merged_df$Score_Diff), ]
+merged_df$Benchmark <- factor(merged_df$Benchmark, levels = merged_df$Benchmark)
+print(merged_df)
 
-ggplot(df_diff, aes(x = Benchmark, y = Score, fill = Score)) +
+# Plot the differences using ggplot2
+ggplot(merged_df, aes(x = Benchmark, y = Score_Diff, fill = Score_Diff)) +
   geom_bar(stat = "identity", position = "dodge") +
   labs(title = "Performance Difference between New and Old Benchmarks",
        x = "Benchmark", y = "Score Difference (ms/op)") +
--- scripts/updateChart.sh
+++ scripts/updateChart.sh
@@ -6,13 +6,13 @@ STARTING_BRANCH=$(git branch --show-current)
 git pull
 ./gradlew build jmhJar
 
-java -jar -Xmx4G -Xms2G build/libs/rosemary-chess-jmh.jar -rf csv -rff new.csv
+java -jar -Xmx4G -Xms2G build/libs/rosemary-chess-jmh.jar .Perft -rf csv -rff new.csv
 mv new.csv docs/data/new.csv
 
 git checkout "$TARGET_BRANCH"
 git pull
 ./gradlew build jmhJar
-java -jar -Xmx4G -Xms2G build/libs/rosemary-chess-jmh.jar -rf csv -rff old.csv
+java -jar -Xmx4G -Xms2G build/libs/rosemary-chess-jmh.jar .Perft -rf csv -rff old.csv
 mv old.csv docs/data/old.csv
 git checkout "$STARTING_BRANCH"
 cd docs
