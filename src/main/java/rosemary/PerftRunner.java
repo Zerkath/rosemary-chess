b7ee900b54959129004025fb20125f8c9654150f
Zerkath <24889977+Zerkath@users.noreply.github.com>
2025-10-18 21:36:50 +0300

    perf: use of stack over recursion saves 10-20ms for perft 6

--- src/main/java/rosemary/PerftRunner.java
+++ src/main/java/rosemary/PerftRunner.java
@@ -1,16 +1,17 @@
 package rosemary;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import rosemary.board.*;
 import rosemary.generation.MoveGenerator;
 import rosemary.types.MoveUtil;
 
 public class PerftRunner {
-
     public static long perft(int depth, boolean print, BoardState boardState) {
         return MoveGenerator.getLegalMoves(boardState)
                 .intStream()
                 .map(move -> {
-                    int result = perftProcess(depth - 1, Mover.makeMove(boardState, (short) move));
+                    int result = perftProcess(boardState, (short) move, depth);
                     if (print) {
                         System.out.println(MoveUtil.moveToString((short) move) + " " + result);
                     }
@@ -19,21 +20,38 @@ public class PerftRunner {
                 .sum();
     }
 
-    private static int perftProcess(int depth, BoardState boardState) {
-        if (depth <= 0) return 1;
-        return MoveGenerator.getLegalMoves(boardState)
-                .intStream()
-                .map(move -> perftProcess(depth - 1, Mover.makeMove(boardState, (short) move)))
-                .sum();
+    private static int perftProcess(BoardState boardState, short initialMove, int depth) {
+        if (depth == 0) return 1;
+        record Node(BoardState state, int depth) {}
+
+        int nodes = 0;
+        Deque<Node> stack = new ArrayDeque<>();
+        stack.push(new Node(Mover.makeMove(boardState, (short) initialMove), depth - 1));
+
+        while (!stack.isEmpty()) {
+            Node current = stack.pop();
+            if (current.depth == 0) {
+                nodes++;
+                continue;
+            }
+
+            for (int move : MoveGenerator.getLegalMoves(current.state)) {
+                BoardState next = Mover.makeMove(current.state, (short) move);
+                stack.push(new Node(next, current.depth - 1));
+            }
+        }
+        return nodes;
     }
 
     /**
-     * Runs perft at set depth & return an array of how many nodes were found & how long the
+     * Runs perft at set depth & return an array of how many nodes were found & how
+     * long the
      * operation took in MS
      *
      * @param depth ply depth
      * @param print whether to print to standard out, useful for debugging
-     * @return long array where index 0 is for number of moves found & index 1 ms the operation took
+     * @return long array where index 0 is for number of moves found & index 1 ms
+     *         the operation took
      */
     public static long[] getPerftScore(int depth, boolean print, BoardState bState) {
         long start = System.currentTimeMillis();
